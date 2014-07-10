5f31459d580639d209bd1db80073a6e27a5bc206
DmAntsybor <anc.dmt@gmail.com>
2014-07-11 01:05:36 +0200

    Issue

---
+++ src/org/training/issueTracker/service/constants/RoleConst.java
@@ -0,0 +1,14 @@
+package org.training.issueTracker.service.constants;
+
+public class RoleConst {
+
+	private RoleConst() {
+		super();
+		
+	}
+	
+	
+	public final static String ADMIN = "admin";
+	public final static String USER = "user";
+	public final static String GUEST = "guest";
+}
